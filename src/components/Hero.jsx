64fba0f0b82a7e566c41897dc21ace65434f5231
tosin adefope <sirtosin45@gmail.com>
2022-05-09 14:08:25 +0100

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div>
       <h1 className='absolute font-bold text-white text-4xl p-2 capitalize font-sans w-full flex justify-center items-center mt-[2em]
-      lg:top-1/2 lg:text-6xl lg:mt-0'>enjoy freedom land</h1>
+      lg:top-1/2 lg:text-6xl lg:mt-0 md:mt-[5em]'>enjoy freedom land</h1>
       <img className='w-full lg:h-[600px] object-cover'  src={image} />
     </div>
   )
